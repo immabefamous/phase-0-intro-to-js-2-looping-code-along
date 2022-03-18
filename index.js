0cfb03d02ff38673bdc8cb994d6b0e8fb0ee4a83
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-17 23:58:06 -0400

    commit tree 3e319f1c6511bbab8a45f214e6cdb27e40cdd122

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const cards2 =[]
+function writeCards(cards) {
+  let x = 0
+  while (x < cards.length) {
+    cards2.push(`Thank you, ${cards[x]}, for the wonderful surprise gift!`)
+    x++;
+  }
+    return cards2
+}
+writeCards(cards2)
+
+
+function countDown(number) {
+        while (number > -1) {
+            console.log(number--);
+        }
+    }
